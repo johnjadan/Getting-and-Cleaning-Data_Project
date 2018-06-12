7f18dbd2f0f1de4df66b7e6a20d4478c9c5d8e60
Peter Johnson <36635675+johnjadan@users.noreply.github.com>
2018-06-11 21:02:55 -0400

    Add files via upload

---
+++ codeanalysis.R
@@ -0,0 +1,51 @@
+#1 Get and merge the data by Loading raw data sets.
+if(!file.exists("./data")){dir.create("./data")}
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileUrl,destfile="./data/Dataset.zip",method="curl")
+unzip(zipfile="./data/Dataset.zip",exdir="./data")
+library(plyr)
+library(data.table)
+
+#2 Organizing Data 
+dataSubject <- rbind(dataSubjectTrain, dataSubjectTest)
+dataActivity<- rbind(dataActivityTrain, dataActivityTest)
+dataFeatures<- rbind(dataFeaturesTrain, dataFeaturesTest)
+names(dataSubject)<-c("subject")
+names(dataActivity)<- c("activity")
+dataFeaturesNames <- read.table(file.path(path_rf, "features.txt"),head=FALSE)
+names(dataFeatures)<- dataFeaturesNames$V2
+
+#3 combining raw data sets into single one data set 
+dataCombine <- cbind(dataSubject, dataActivity)
+Data <- cbind(dataFeatures, dataCombine)
+
+#4 Extract only the measurements on the mean and standard deviation for each measurement
+subdataFeaturesNames<-dataFeaturesNames$V2[grep("mean\\(\\)|std\\(\\)", dataFeaturesNames$V2)]
+selectedNames<-c(as.character(subdataFeaturesNames), "subject", "activity" )
+Data<-subset(Data,select=selectedNames)
+
+str(Data)
+
+#5 Appropriately label the data set with descriptive activity names.
+activityLabels <- read.table(file.path(path_rf, "activity_labels.txt"),header = FALSE)
+names(Data)<-gsub("^t", "time", names(Data))
+names(Data)<-gsub("^f", "frequency", names(Data))
+names(Data)<-gsub("Acc", "Accelerometer", names(Data))
+names(Data)<-gsub("Gyro", "Gyroscope", names(Data))
+names(Data)<-gsub("Mag", "Magnitude", names(Data))
+names(Data)<-gsub("BodyBody", "Body", names(Data))
+names(Data)
+
+#6 Creates a second, independent tidy data set with the average of each variable for 
+#each activity and each subject
+
+library(plyr);
+Data2<-aggregate(. ~subject + activity, Data, mean)
+Data2<-Data2[order(Data2$subject,Data2$activity),]
+write.table(Data2, file = "tidydata.txt",row.name=FALSE)
+
+data.table(Data2)
+
+
+
+
